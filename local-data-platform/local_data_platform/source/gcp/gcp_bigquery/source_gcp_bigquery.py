b97aa0b665a1f6e877000c07a1bdfc2360e2da73
Mrutunjay Kinagie <mrutunjay.kinagi@gmail.com>
2024-10-02 00:43:08 +0530

    Wed 2 Oct 12:42 am #19 - Added source class fror near GCP BigQuery

--- local-data-platform/local_data_platform/source/gcp/gcp_bigquery/source_gcp_bigquery.py
+++ local-data-platform/local_data_platform/source/gcp/gcp_bigquery/source_gcp_bigquery.py
@@ -1,9 +1,23 @@
-from google.cloud import bigquery
+from source.gcp.gcp_connection import GCPBigQueryConnection
 
-class SourceGCPBigQuery :
+class SourceGCPBigQuery:
+    """
+    A class to interact with Google BigQuery.
+    client : GCPBigQueryConnection
+        An instance of GCPBigQueryConnection to interact with BigQuery.
+    """
+    def __init__(self):
+        self.client = GCPBigQueryConnection('path/to/credentials.json', 'your-project-id')
 
-    def fetch_data(query):
-        client = bigquery.Client()
-        query_job = client.query(query)
+    def fetch_data(self, query):
+        """
+        Executes a given SQL query on Google BigQuery and returns the result as a pandas DataFrame.
+
+        Args:
+            query (str): The SQL query to be executed.
+
+        Returns:
+            pandas.DataFrame: The result of the query as a DataFrame.
+        """
+        query_job = self.client.query(query)
         return query_job.to_dataframe()
-    
--- local-data-platform/local_data_platform/source/near/source_near_gcp.py
+++ local-data-platform/local_data_platform/source/near/source_near_gcp.py
@@ -1,11 +1,23 @@
-from local_data_platform.source.gcp.gcp_bigquery import SourceGCPBigQuery
+from local_data_platform.source.gcp.gcp_bigquery.source_gcp_bigquery import SourceGCPBigQuery
+"""
+This module defines the Source_Near_GCP class, which inherits from SourceGCPBigQuery
+and provides functionality to fetch transaction data from the NEAR protocol on Google Cloud Platform.
 
+Classes:
+    Source_Near_GCP: A class to fetch NEAR protocol transaction data from GCP BigQuery.
+
+Methods:
+    fetch_near_transaction(self):
+        Fetches NEAR protocol transaction data from the previous day.
+        Returns:
+            DataFrame: A pandas DataFrame containing the transaction data.
+"""
 class Source_Near_GCP(SourceGCPBigQuery):
 
     def fetch_near_transaction(self):
         query = """
             SELECT *
-            FROM `nearprotocol.near.transaction`
-            WHERE DATE(block_timestamp) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+            FROM `bigquery-public-data.crypto_near.transactions`
+            LIMIT 1000;
         """
-        return self.fetch(query)
+        return self.fetch_data(query)
