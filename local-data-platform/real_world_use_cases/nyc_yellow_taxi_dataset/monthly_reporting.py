155c01b2f21ef7f077d461b2316c5cbdea6d48d7
Tushar Choudhary <151359025+tusharchou@users.noreply.github.com>
2024-10-24 21:13:24 +0530

    added get nyc taxi data function to monthly report
    Thu Oct 24 9:15 PM IST

--- local-data-platform/real_world_use_cases/nyc_yellow_taxi_dataset/monthly_reporting.py
+++ local-data-platform/real_world_use_cases/nyc_yellow_taxi_dataset/monthly_reporting.py
@@ -15,6 +15,7 @@ HVFHV files will now include 17 more columns (please see High Volume FHV Trips D
 Additional columns will be added to the old files as well.
 '''
 from local_data_platform.pipeline.ingestion.pyarrow.parquet_to_iceberg import ParquetToIceberg
+from local_data_platform.pipeline.egression.iceberg_to_csv import IcebergToCSV
 from local_data_platform import Config, SupportedFormat
 from local_data_platform.store.source.json import Json
 from local_data_platform.exceptions import PipelineNotFound
@@ -23,6 +24,46 @@ from local_data_platform.logger import log
 
 
 logger = log()
+
+
+def get_nyc_yellow_taxi_dataset(
+        dataset='nyc_taxi',
+        config_path='/real_world_use_cases/nyc_yellow_taxi_dataset/config/egression.json'
+):
+    logger.info(
+        """
+        We will try to read a ICEBERG table from local catalog 
+        """
+    )
+
+    config = Config(
+        **Json(
+            name=dataset,
+            path=os.getcwd()+config_path,
+        ).get()
+    )
+
+    logger.info(
+        f"""
+        We are using the following dictionary as the configuration to generate a monthly trust metric
+        {config}
+        """
+    )
+    if (
+            config.metadata['source']['format'] == SupportedFormat.ICEBERG.value and
+            config.metadata['target']['format'] == SupportedFormat.CSV.value
+    ):
+        data_loader = IcebergToCSV(config=config)
+        data_loader.load()
+    else:
+        raise PipelineNotFound(
+            f"""
+            source {config.metadata['source']['format']} 
+            to target {config.metadata['target']['format']}
+            pipeline is not supported yet
+            """
+        )
+
 
 
 def put_nyc_yellow_taxi_dataset(
@@ -65,4 +106,5 @@ def put_nyc_yellow_taxi_dataset(
         )
 
 
-put_nyc_yellow_taxi_dataset()
+# put_nyc_yellow_taxi_dataset()
+get_nyc_yellow_taxi_dataset()
