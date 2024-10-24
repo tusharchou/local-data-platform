c7e70a8f4788a00545ccd8ef07cce2c54346c796
Tushar Choudhary <151359025+tusharchou@users.noreply.github.com>
2024-10-24 21:25:43 +0530

    added parquet to iceberg egression class
    Thu Oct 24 9:25 PM IST

---
+++ local-data-platform/local_data_platform/pipeline/ingestion/parquet_to_iceberg/__init__.py
@@ -0,0 +1,31 @@
+from local_data_platform.pipeline.egression import Egression
+from pyarrow import parquet, Table
+from local_data_platform.format.parquet import Parquet
+from local_data_platform.format.iceberg import Iceberg
+from local_data_platform import Config
+from local_data_platform.logger import log
+
+
+logger = log()
+
+
+class ParquetToIceberg(Egression):
+
+    def __init__(self, config: Config, *args, **kwargs):
+        self.source = config.metadata['source']
+        self.target = config.metadata['target']
+        self.source = Parquet(
+            name=self.source['name'],
+            path=self.source['path']
+        )
+        self.target = Iceberg(
+            name=self.target['name'],
+            catalog=self.target['catalog']
+        )
+        logger.info(
+            f"""
+            ParquetToIceberg initialised with
+            source {self.source}
+            target {self.target}
+            """
+        )
