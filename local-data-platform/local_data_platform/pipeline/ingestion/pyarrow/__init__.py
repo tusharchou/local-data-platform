ccf4c0d967411e1de8b3f4e7165484755211e894
Tushar Choudhary <151359025+tusharchou@users.noreply.github.com>
2024-10-24 18:53:26 +0530

    fixed extract function to actually return df Thu Oct 24 6:55 PM IST

--- local-data-platform/local_data_platform/pipeline/ingestion/pyarrow/__init__.py
+++ local-data-platform/local_data_platform/pipeline/ingestion/pyarrow/__init__.py
@@ -1,19 +1,21 @@
 from local_data_platform.pipeline.ingestion import Ingestion
-from pyarrow import parquet, Table
-from local_data_platform.store.source.parquet import Parquet
-from local_data_platform.store.target.iceberg import Iceberg
+from pyarrow import Table
 from local_data_platform import Config
+from local_data_platform.logger import log
+import os
 
-class PyArrow(Ingestion):
+
+logger = log()
+
+
+class PyArrowLoader(Ingestion):
 
     def __init__(self, config: Config, *args, **kwargs):
         self.config = config
-        self.source = Parquet(**config.metadata['source'])
-        self.target = Iceberg(**config.metadata['target'])
 
-    def extract(self) -> Table:
-        self.df = parquet.read_table(self.source.path)
-
+    def _extract(self) -> Table:
+        return self.source.get()
 
     def load(self):
-        self.target.put(self.df.schema)
+
+        self.target.put(self._extract())
