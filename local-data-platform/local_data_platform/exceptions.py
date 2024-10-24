7c1bd0dc5d2a5c41bda5c2b365b2cc362d95ed31
Tushar Choudhary <151359025+tusharchou@users.noreply.github.com>
2024-10-24 18:01:07 +0530

    added PipelineNotFound exception Thu Oct 24 6:05 PM IST

--- local-data-platform/local_data_platform/exceptions.py
+++ local-data-platform/local_data_platform/exceptions.py
@@ -1,2 +1,5 @@
 class TableNotFound(Exception):
     """Raised when accessing a table that doesn't exist"""
+
+class PipelineNotFound(Exception):
+    """Raised when running a pipeline that doesn't exist"""
