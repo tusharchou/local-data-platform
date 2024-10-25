0b92abeb90b4d0062e5952f42da5dc10e6c3b8fb
unknown <amit220698@gmail.com>
2024-10-25 18:38:07 +0530

    folder structure for dbt transformation

---
+++ local-data-platform/local_data_platform/transformations/dbt_models/silver/silver.sql
@@ -0,0 +1,10 @@
+-- gold/silver_to_gold.sql
+with silver_data as (
+    select * from {{ ref('bronze_to_silver') }}
+)
+select 
+    neighbourhood_group, 
+    avg(price) as avg_price, 
+    count(id) as total_listings
+from silver_data
+group by neighbourhood_group
