0232ac4f396dae83f0ffd600a60b5bc626589964
Tushar Choudhary <151359025+tusharchou@users.noreply.github.com>
2024-10-30 01:49:10 +0530

    fixed bug warehouse uri
    Thu Oct 24 9:25 PM IST

--- local-data-platform/local_data_platform/catalog/local/iceberg/__init__.py
+++ local-data-platform/local_data_platform/catalog/local/iceberg/__init__.py
@@ -30,10 +30,10 @@ class LocalIcebergCatalog(SqlCatalog):
 
     def __init__(self, name: str, path: str, *args, **kwargs):
         self.name = name
-        self.uri = f"sqlite:///{path}/{name}.db" 
+        self.uri = f"sqlite:///{path}/{name}_catalog.db"
         self.warehouse = f"file://{path}"
         try: 
-            logger.error(f"Initializing LocalIcebergCatalog with {self.uri}")
+            logger.info(f"Initializing LocalIcebergCatalog with {self.uri}")
             super().__init__(*args, **kwargs, **self.__dict__)
         except Exception as e:
             logger.error(f"Failed to initialize LocalIcebergCatalog {e}")
