f3d0d1fd5196db90e32dfc3d7c79c09b47e7e89c
Arpita Dhir <93266146+ArpitaDhir@users.noreply.github.com>
2024-10-29 17:17:06 +0530

    Update partner.py
    PRD updated

--- partner.py
+++ partner.py
@@ -1,3 +1,24 @@
+"""
+Product Requirements Doc
+
+Product? 
+- Users are people who want to find and learn new recipes 
+- Recipe is an explanation of how a dish is made. It has:
+ 	- ingredients: quantity, ingredient name 
+  	- process
+   	- prep time: minutes
+    	- cook time: minutes
+     	- difficulty: easy, intermediate, hard
+      	- course: starter, main course, dessert, beverage
+       	- Price: Rupees
+- Offline Dinners
+	- Register: Must be signed in to attend event 
+ 	- Dinner Name
+  	- Cuisine
+   	- Chef Name
+    	- Menu: All dishes available for dinner
+ 	- Price: Rupees
+"""
 from local_data_platform import helloworld
 from local_data_platform.entity import User, Product
 from local_data_platform.transaction import Transaction 
